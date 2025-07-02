8ba0c207cc884a534373c2ef7f2c7b7ac225a0f3
Taran D <taran184@gmail.com>
2025-07-01 21:51:18 -0700

    Token definition

---
+++ src/Token.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+enum class TokenType {
+    // keywords
+    DEF,
+    IF,
+    ELSE,
+    FOR,
+    EXTERN,
+
+    // binary operators
+    PLUS,
+    MINUS,
+    LT,
+    LE,
+    GT,
+    GE,
+
+    // misc
+    LPAR,
+    RPAR,
+    SEMICOLON,
+};
+
+class Token {
+public:
+    Token(TokenType type, std::string data, int line, int col);
+};
