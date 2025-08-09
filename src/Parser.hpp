033ee69aa838a7a1d3b28acb98b584df19678c7d
Taran D <taran184@gmail.com>
2025-08-08 20:01:05 -0700

    change parser to take ownership of the tokens vector

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -7,7 +7,7 @@
 
 class Parser {
 private:
-    std::vector<Token>& tokens;
+    std::vector<Token> tokens;
     std::size_t pos;
     Token end_token;  // dummy end token to use as current token if pos exceeds the size of tokens vector
     int num_errors;
@@ -38,8 +38,8 @@ private:
     std::unique_ptr<IfExpr> parseIfExpr();
 
 public:
-    Parser(std::vector<Token>& tokens)
-        : tokens(tokens), pos(0), end_token(Token(TokenType::END_PROG, "", 0, 0)), num_errors(0) {}
+    Parser(std::vector<Token> tokens)
+        : tokens(std::move(tokens)), pos(0), end_token(Token(TokenType::END_PROG, "", 0, 0)), num_errors(0) {}
 
     std::unique_ptr<Program> Parse();
 };
